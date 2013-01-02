11de6fb829144c01e0cdbb3d3e32f87c4045c0c8
Bernie Simon <bernie.simon@gmail.com>
2013-01-02 09:02:29 -0500

    Revised demo script to expect command script under base directory

--- dsldemo.pl
+++ dsldemo.pl
@@ -5,17 +5,14 @@ use warnings;
 
 use FindBin qw($Bin);
 use lib $Bin;
-
 use DSLMain;
 
 my $base_dir = $Bin;
-my $script;
 
-##if (@ARGV) {
-##    $script = shift @ARGV or die "No script on command line\n";
-##    $script = "$base_dir/scripts/$script";
-##    unshift(@ARGV, $script);
-##}
+if (@ARGV) {
+    my $script = "$base_dir/scripts/" . shift(@ARGV);
+    unshift(@ARGV, $script);
+}
 
 my $demo = DSLMain->new(stage_dir => "$base_dir/stage");
 $demo->main(@ARGV);
