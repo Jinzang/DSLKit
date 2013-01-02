25d0f070b374cdf9aec40c9adb154b2f10d56e21
Bernie Simon <bernie.simon@gmail.com>
2013-01-02 09:05:55 -0500

    Improved the documentation to bring it up to Perl standards

--- DSLMain.pm
+++ DSLMain.pm
@@ -170,11 +170,21 @@ __END__
 
 DSLMain -- Top level for DSL scripts
 
-=head1 USAGE
+=head1 SYNOPSIS
+
+    use DSLMain;
+    my $script = shift @ARGV;
+    my $demo = DSLMain->new(stage_dir => "/tmp");
+    $demo->main($script, @ARGV);
+
+=head1 DESCRIPTION
 
 DSL scripts are line delimeted lists of commands. Blanks lines and all
 characters following a sharp character are ignored. Some commands are blocks
-of lines, in which case the block is terminated by an end on its own line.
+of lines, in which case the block is terminated by an end on its own line. 
+Lines can contain subcommands, which are enclosed in square brackets. These
+subcommands are run first and the replaced by their return value.
+
 Each command is implemented by a package whose name is the capitalized command
 name followed by the word 'Command'. Commands are divided into core language
 commands, which implement the method intereperet_some_lines (even if they are
@@ -183,14 +193,61 @@ script and implement the execute or run method. The language commands are
 
 =over
 
+=item debug - Stop and accept new commands from terminal
+
 =item for - Loop over a block of commands
 
-=item log - Write a message to log
+=item log - Write a message to log and mail log to user
 
 =item macro - Define a block of commands as a macro
 
-=item new - define parameters for a command and initialize it
+=item mock - A fake command for testing
+
+=item new - Define parameters for a command and initialize it
+
+=item show - Get the value of a variable field
 
 =back
 
-Commands are explained in more details in the files that implement them.
+Commands are explained in more details in the files that implement them. New
+commands should inherit from one of the base classes, all which begin with DSL.
+These classes are:
+
+=over
+
+=item DSLVar - For single line commands and the base class for all commands
+
+=item DSLCmd - For single line commands that log a message when they run
+
+=item DSLBlock - For multiple line commands whose following lines are data
+
+=item DSLCode - For multiple line commands whose following lines are commands
+
+=back
+
+The documentation in these classes explain how to subclass them. I expect that
+this code will not entirely meet your needs and you will need to modify it
+before using it. This is especially true of this class, as it is the interface
+that connects with the rest of your script.
+
+=head1 METHODS
+
+=head2 new
+
+    my $obj = DSLMain->new(var1 => 'value1', var2 => 'value2')
+
+The new method creates a new top level object. The top level object contains
+all the named variables in the script, the log messages, and the script status.
+The argument to the new method is a hash containing pre-defined variables that
+your script will use. The DSLMain method as written expects the variable
+stage_dir, which is used to create a directory to hold temporary files used by
+the script. If it is not present, the directory will not be created.
+
+=head2 main
+
+    $obj->main($filename, @args);
+    
+The main method should be called after creating the object with new. The first
+argument is a filename of a script containing command lines to be interpreted.
+The remaining arguments should be scalars. They can be accessed in the script
+using numbered variables ($1, $2, ...) or as a whole with $*.
