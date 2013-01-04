bb01ce0fcdadb881a6b8eae280b7152714043066
Bernie Simon <bernie.simon@gmail.com>
2013-01-04 07:55:06 -0500

    Removed unused external commands

--- ExternalCommands.pm
+++ ExternalCommands.pm
@@ -9,23 +9,10 @@ package ExternalCommands;
 
 use Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(CONVERT_COMMAND DVIPDF_COMMAND LATEX_COMMAND
-                 MAIL_COMMAND NAME_COMMAND);
-
-use constant CONVERT_COMMAND => '/usr/bin/convert';
-#use constant CONVERT_COMMAND => '/usr/local/bin/convert';
-
-use constant DVIPDF_COMMAND => '/usr/bin/dvipdfm';
-#use constant DVIPDF_COMMAND => '/usr/texbin/dvipdfm';
-
-use constant LATEX_COMMAND => '/usr/bin/latex';
-#use constant LATEX_COMMAND => '/usr/texbin/latex';
+our @EXPORT = qw(MAIL_COMMAND NAME_COMMAND);
 
 use constant MAIL_COMMAND => '/usr/lib/sendmail -oi -t';
-#use constant MAIL_COMMAND => '';
-
 use constant NAME_COMMAND => '/bin/uname -n';
-#use constant NAME_COMMAND => '/usr/bin/uname -n';
 
 1;
 __END__
@@ -33,8 +20,7 @@ __END__
 
 Commands
 
-=head1 USAGE
+=head1 DESCRIPTION
 
 This package contains the constants for the paths of external commands, which
 may differ between systems. 
-=back
