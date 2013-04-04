8b0d2efa81a07cb2e85df0d4829321d042051dcf
Bernie Simon <bernie.simon@gmail.com>
2013-04-04 19:29:03 -0400

    Added split_lines, to make debugging more pleasant

--- DSLMock.pm
+++ DSLMock.pm
@@ -17,10 +17,19 @@ sub main {
     my ($pkg, $code, @args) = @_;
 
     my $self = $pkg->new();
-    my @lines = map {"$_\n"} split(/\n/, $code);
+    my @lines = $self->split_lines($code);
     my $reader = LineReader->new(\@lines);
 
     return $self->parse_some_lines($reader, $self, @args);
+}
+
+#-----------------------------------------------------------------------
+# Split code into lines
+
+sub split_lines {
+    my ($self, $code) = @_;
+    
+    return map {"$_\n"} split(/\n/, $code);
 }
 
 1;
